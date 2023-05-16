e9a6be98a2968b8180747e85cf2efe5051c2115e
Erick Delfin <erickdelfinmolina@gmail.com>
2023-05-15 20:53:19 -0700

    Update SWAGGA config for deploy

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,17 @@ async function bootstrap() {
     .setVersion(version)
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Have to set up custom JS/CSS for Swagger in order for it to
+  // work on Vercel deployment. https://stackoverflow.com/questions/73960298/nestjs-swagger-css-not-loading-when-deployed-to-vercel
+  SwaggerModule.setup('api', app, document, {
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css',
+    ],
+  });
 
   await app.listen(APP_PORT);
   Logger.verbose(`App is now running on port=${APP_PORT}`);
