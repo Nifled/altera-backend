6e9c4fba1ffd3286b27be7dc77ccbaf02d23c5bb
Erick Delfin <erickdelfinmolina@gmail.com>
2023-05-17 14:53:22 -0700

    Create a JWT strategy using passport + add a login endpoint + create jwt auth guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthEntity } from './entities/auth.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async login(email: string, password: string): Promise<AuthEntity> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException(`User not found with email: ${email}.`);
+    }
+
+    if (user.password !== password) {
+      throw new UnauthorizedException('Invalid password.');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
+}
