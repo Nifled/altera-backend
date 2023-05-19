e51b3e2b4690a880cdba62ec6ff0f4c7a71e6a10
Erick Delfin <erickdelfinmolina@gmail.com>
2023-05-19 15:27:30 -0700

    Add more validation to create DTO objs

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { IsValidUserId } from 'src/users/validators/user-id-exists.validator';
 
 export class CreatePostDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   caption: string;
 
+  @IsString()
   @ApiProperty()
   @IsValidUserId()
   authorId: string;
