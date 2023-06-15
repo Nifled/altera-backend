145505919b64e66eb626eeef64510940078b2071
Erick Delfin <erickdelfinmolina@gmail.com>
2023-06-15 15:02:34 -0700

    Set up query logging on prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,34 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
   // onModuleInit is optional — if left out, Prisma will
   // connect lazily on its first call to the database.
   async onModuleInit() {
+    this.$on<any>('query', (event: Prisma.QueryEvent) => {
+      this.logger.debug(`Query: ${event.query}`);
+      this.logger.debug(`Query Duration: ${event.duration} ms`);
+    });
+
     await this.$connect();
   }
 
