dbb8aaaf8fab179218fe435fb2cfcd714a9e316a
Erick Delfin <erickdelfinmolina@gmail.com>
2023-06-30 15:50:15 -0700

    Add tests for storage service

--- src/storage/storage.service.spec.ts
+++ src/storage/storage.service.spec.ts
@@ -1,6 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StorageService } from './storage.service';
 
+const mockS3Instance = {
+  send: jest.fn().mockReturnValue({ $metadata: { httpStatusCode: 200 } }),
+  promise: jest.fn(),
+};
+
+jest.mock('@aws-sdk/client-s3', () => {
+  return {
+    S3Client: jest.fn(() => mockS3Instance),
+    PutObjectCommand: jest.fn(),
+  };
+});
+
 describe('StorageService', () => {
   let service: StorageService;
 
@@ -12,7 +24,30 @@ describe('StorageService', () => {
     service = module.get<StorageService>(StorageService);
   });
 
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('uploadFile()', () => {
+    const testFile: Partial<Express.Multer.File> = {
+      mimetype: 'image/jpeg',
+      buffer: Buffer.from('This is a test file'),
+    };
+
+    it('should successfully upload a file and return image url', async () => {
+      const clientSpy = jest.spyOn(mockS3Instance, 'send');
+      const imageUrl = await service.uploadFile(
+        testFile as Express.Multer.File,
+        'test',
+      );
+
+      expect(clientSpy).toBeCalledTimes(1);
+      expect(imageUrl).toStartWith('https://');
+      expect(imageUrl).toEndWith('test');
+    });
   });
 });
--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -14,6 +14,7 @@ interface StorageProvider {
 export class StorageService implements StorageProvider {
   private client: S3Client;
   private region: string;
+  private bucket: string;
   private logger = new Logger(StorageService.name);
 
   constructor() {
@@ -24,13 +25,11 @@ export class StorageService implements StorageProvider {
    * Upload a file to storage provider
    * @param file The file to upload
    * @param key The key of the file
-   * @returns The url of the file
+   * @returns The url of the uploaded file
    */
   async uploadFile(file: Express.Multer.File, key: string): Promise<string> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const bucket = process.env.AWS_S3_BUCKET;
     const input: PutObjectCommandInput = {
-      Bucket: bucket,
+      Bucket: this.bucket,
       Body: file.buffer,
       Key: key,
       ContentType: file.mimetype,
@@ -40,8 +39,8 @@ export class StorageService implements StorageProvider {
     try {
       const response = await this.client.send(new PutObjectCommand(input));
 
-      if (response.$metadata.httpStatusCode === 200) {
-        return `https://${bucket}.s3.${this.region}.amazonaws.com/${key}`;
+      if (response?.$metadata?.httpStatusCode === 200) {
+        return `https://${this.bucket}.s3.${this.region}.amazonaws.com/${key}`;
       }
 
       // If there is other status code other than 200, the image was not uploaded
@@ -53,7 +52,12 @@ export class StorageService implements StorageProvider {
   }
 
   private createProviderInstance() {
-    this.region = process.env.AWS_S3_REGION || 'us-west-1';
+    // TODO: Fix this messy ass shit
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.bucket = process.env.AWS_S3_BUCKET!;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.region = process.env.AWS_S3_REGION!;
+
     this.client = new S3Client({
       region: this.region,
       credentials: {
